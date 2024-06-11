421b6ad11114806ac6ec477b3d34eefdbdbed16a
tecKaleAnders <143652890+tecKaleAnders@users.noreply.github.com>
2024-06-10 22:57:37 -0500

    connect to backend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+Amplify.configure(outputs);
+
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+export default withAuthenticator(App);
